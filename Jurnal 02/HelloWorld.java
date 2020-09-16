80b52b9f6a79c6129d0c583002b21a575a1c8b7a
mhmadidris <idris3097@gmail.com>
2020-09-16 13:20:57 +0700

    HelloWorld.java

---
+++ Jurnal 02/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+	public static void main (String[] args) {
+		System.out.println ("Nama: Muhamad Idris");
+		System.out.println ("NIM: 6706202008");
+		System.out.println ("Kelas: D3IF44-04");
+	}
+}
